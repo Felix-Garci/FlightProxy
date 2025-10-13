5ead47da79824e4fb06b759b0be43c910d739579
Felix <202006933@alu.comillas.edu>
2025-10-13 18:54:08 +0200

    Tcp msp placeholder functions , no content

--- src/net/TcpMspEndpoint.cpp
+++ src/net/TcpMspEndpoint.cpp
@@ -1,1 +1,35 @@
+#include "TcpMspEndpoint.hpp"
 // placeholder
+
+fcbridge::net::TcpMspEndpoint::TcpMspEndpoint(msp::MspSessionManager *session)
+{
+    session_ = session;
+}
+bool fcbridge::net::TcpMspEndpoint::start()
+{
+    // placeholder
+    return true;
+}
+void fcbridge::net::TcpMspEndpoint::stop()
+{
+    // placeholder
+}
+bool fcbridge::net::TcpMspEndpoint::sendToClient(const msp::MspFrame &resp)
+{
+    // placeholder
+    return true;
+}
+
+void fcbridge::net::TcpMspEndpoint::onTcpBytes(std::span<const uint8_t> bytes)
+{
+    // placeholder
+}
+void fcbridge::net::TcpMspEndpoint::setLowLevelSender(SendFunc f)
+{
+    sendRaw_ = f;
+}
+
+void fcbridge::net::TcpMspEndpoint::handleIncomingFrame(const msp::MspFrame &f)
+{
+    // placeholder
+}
