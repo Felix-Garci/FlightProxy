4be50c216aba98d8af03b6cfb98fabfe8e6054dd
Felix <202006933@alu.comillas.edu>
2025-10-14 12:08:56 +0200

    guardamos en drivers_test-txt el main de prueva dejamos el main anterior como0 estaba

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,4 @@
-// main.cpp
-#include "hal/uart.hpp"
-#include "hal/wifi.hpp"
-#include "hal/tcp.hpp"
-#include "hal/udp.hpp"
-#include "utils/log.hpp"
-
-extern "C" void app_main(void)
-{
-    SimpleUARTEvt uart;
-
-    // Ajusta pines/puerto a tu hardware
-    uart.begin(UART_NUM_0, GPIO_NUM_1, GPIO_NUM_3, 115200, 1024, 10);
-
-    // Callback que se llama cada vez que llega data
-    uart.onReceive([&](const uint8_t *data, size_t len)
-                   {
-        // Aquí estás en contexto de task (seguro para llamar API de IDF/FreeRTOS)
-        uart.write(reinterpret_cast<const uint8_t *>("RX: "), 4);
-        uart.write(data, len);
-        uart.write("\r\n"); });
-
-    WiFiSTA wifi;
-    if (wifi.begin("Sup", "rrrrrrrr", 15000, 112) == ESP_OK)
-    {
-        uart.write("Wi-Fi connected!\r\n");
-        uart.write(wifi.ipStr().c_str());
-    }
-    else
-    {
-        uart.write("Wi-Fi connection failed!\r\n");
-        return;
-    }
-
-    SimpleTCPServer srv;
-
-    // 1) Inicia el servidor en el puerto 12345
-    if (srv.begin(12345) != ESP_OK)
-    {
-        printf("No se pudo iniciar el servidor\n");
-        return;
-    }
-
-    // 2) Callback de recepción
-    srv.onReceive([&](int sock, const uint8_t *data, size_t len)
-                  {
-                      // Echo: devuelve lo mismo
-                      srv.write(data, len);
-                      // Si quieres tratar por líneas:
-                      // - acumula en un buffer propio hasta encontrar '\n'
-                  });
-
-    srv.start(4096, tskIDLE_PRIORITY + 2);
-
-    SimpleUDPReceiver udp;
-
-    udp.begin(5005, [&](const uint8_t *data, size_t len, const sockaddr_in &from)
-              {
-        char ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(from.sin_addr), ip, sizeof(ip));
-        char line[256];
-        int n = snprintf(line, sizeof(line),
-                         "UDP desde %s:%d -> %.*s\r\n",
-                         ip, (int)ntohs(from.sin_port), (int)len, (const char *)data);
-
-        if (n > 0)
-            uart.write(line); });
-
-    // Tu app puede seguir haciendo otras cosas...
-    for (;;)
-    {
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
-
-/*#include "net/TcpMspEndpoint.hpp"
+#include "net/TcpMspEndpoint.hpp"
 #include "msp/MspSessionManager.hpp"
 #include "msp/MspPassthroughProxy.hpp"
 #include "msp/LocalCtrlHandler.hpp"
@@ -111,4 +36,3 @@ int app_main(void)
 
     return 0;
 }
-*/
