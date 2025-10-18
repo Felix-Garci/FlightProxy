f6b7611db4c70ca59d6854c14ea24e8fdea475ba
Felix <202006933@alu.comillas.edu>
2025-10-18 19:12:47 +0200

    main first wiring

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,78 @@
-#include "net/TcpMspEndpoint.hpp"
-#include "msp/MspSessionManager.hpp"
-#include "msp/MspPassthroughProxy.hpp"
-#include "msp/LocalCtrlHandler.hpp"
-#include "rc/RcChannels.hpp"
-#include "rc/RcDefaults.hpp"
-#include "state/StateModel.hpp"
-#include "ibus/IbusTransmitter.hpp"
-#include "utils/Log.hpp"
-
-#include "hal/wifi.hpp"
-#include "hal/tcp.hpp"
-
 #include <cstdint>
 #include <cstddef>
 #include <span>
 
-using namespace fcbridge;
+#include "Structure.hpp"
+#include "utils/Log.hpp"
+
+#include "hal/wifi.hpp"
+#include "hal/tcp.hpp"
+#include "hal/udp.hpp"
+#include "hal/uart.hpp"
+
+#include "Interface/Client.hpp"
+#include "Interface/FC.hpp"
+#include "Interface/FastRC_RX.hpp"
+#include "Interface/FastRC_TX.hpp"
 
 extern "C" void app_main(void)
 {
     // Logs
-    utils::Log::init("transponder");
+    tp::UTILS::Log::init("transponder");
 
-    // Instancias
-    rc::RcChannels rc;
-    state::StateModel st;
-    msp::LocalCtrlHandler ctrl;
-    msp::MspSessionManager session;
-    msp::MspPassthroughProxy proxy(&session);
-    net::TcpMspEndpoint tcp(&session);
-    ibus::IbusTransmitter ibus;
-
-    // Inicialización RC por defecto
-    rc.init(rc::defaults::stoped());
-
-    // Wiring
-    ctrl.setRc(&rc);
-    ctrl.setState(&st);
-    session.setCtrlHandler(&ctrl);
-    session.setProxy(&proxy);
-    session.setTcpEndpoint(&tcp);
-    ibus.setChannelsSource(&rc);
-    ibus.setStateModel(&st);
-
+    // HAL Creation
     // WIFI
-    WiFiSTA wifi;
+    tp::HAL::WiFiSTA wifi;
     if (wifi.begin("Sup", "rrrrrrrr", 15000, 112) != ESP_OK)
-        utils::Log::error("Wi-Fi connection failed");
+    {
+        tp::UTILS::Log::error("Wi-Fi connection failed");
+    }
 
     // TCP para recivir mensajes msp
-    SimpleTCPServer srv;
+    tp::HAL::SimpleTCPServer srv;
     if (srv.begin(12345) != ESP_OK)
-        utils::Log::error("TCP server start failed");
-
-    // Este error es por el vscode, en vd no esta mal
+    {
+        tp::UTILS::Log::error("TCP server start failed");
+    }
+    // INTERFAZ
+    tp::I::Client IClient(srv);
     srv.onReceive([&](int, const uint8_t *data, std::size_t len)
-                  { utils::Log::info("TCP RX %zu bytes", len);
-                    tcp.onTcpBytes(std::span<const uint8_t>(data, len)); });
+                  { IClient.recive(data, len); });
 
-    tcp.setLowLevelSender([&](std::span<const uint8_t> bytes)
-                          { utils::Log::info("TCP TX %zu bytes", bytes.size());
-                            return srv.write(bytes.data(), bytes.size()) == bytes.size(); });
+    // Uart A para MSP FC
+    tp::HAL::SimpleUARTEvt uartA;
+    if (uartA.begin(UART_NUM_1, GPIO_NUM_18, GPIO_NUM_19, 115200, 1024, 10) != ESP_OK)
+    {
+        tp::UTILS::Log::error("UARTA start failed");
+    }
+    // INTERFAZ
+    tp::I::FC IFC(uartA);
 
+    // UDP para recivir RC
+    tp::HAL::SimpleUDPReceiver udp;
+    if (udp.begin(5005) != ESP_OK)
+    {
+        tp::UTILS::Log::error("UDP start failed");
+    }
+    // INTERFAZ
+    tp::I::FastRC_RX IFastRecive(udp);
+    udp.onReceive([&](const uint8_t *data, size_t len,
+                      const sockaddr_in)
+                  { IFastRecive.recive(data, len); });
+
+    // Uart B para IBus FC
+    tp::HAL::SimpleUARTEvt uartB;
+    if (uartB.begin(UART_NUM_2, GPIO_NUM_20, GPIO_NUM_21, 115200, 1024, 10) != ESP_OK)
+    {
+        tp::UTILS::Log::error("UARTB start failed");
+    }
+    // INTERFAZ
+    tp::I::FastRC_TX IFastSender(uartB);
+
+    // Arrancamos servidor TCP
     srv.start(4096, tskIDLE_PRIORITY + 2);
 
-    utils::Log::info("System initialized");
+    tp::UTILS::Log::info("System initialized");
 
     for (;;)
         vTaskDelay(pdMS_TO_TICKS(1000));
