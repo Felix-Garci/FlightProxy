cc1fab707aea7b93e25c54ef56bbf9bc3dc0cb06
Felix <202006933@alu.comillas.edu>
2025-10-18 16:39:23 +0200

    start structure

---
+++ src/Structure.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace tp
+{
+    namespace I
+    {
+        // Parent
+        class Interface;
+
+        // Children
+        class Client;
+        class FastRC_RX;
+        class FC;
+        class FastRC_TX;
+    }
+    namespace T
+    {
+        // Parent
+        class Transaction;
+
+        // Children
+        class Client;
+        class FC;
+    }
+
+}
