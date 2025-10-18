50f00c60b84d5ac7fce98241975ab83b87cee7c7
Felix <202006933@alu.comillas.edu>
2025-10-18 18:33:49 +0200

    msp msg included to structure

--- src/Structure.hpp
+++ src/Structure.hpp
@@ -1,7 +1,25 @@
 #pragma once
+#include <cstdint>
+#include <span>
+#include <vector>
 
 namespace tp
 {
+    namespace MSG
+    {
+        struct Frame
+        {
+            uint8_t flag = 0;             // Flag del mensaje
+            uint16_t cmd = 0;             // ID MSP
+            std::vector<uint8_t> payload; // datos crudos
+            uint8_t result = 0;           // código de resultado (solo en respuestas)
+        };
+
+        bool decode(const std::vector<uint8_t> &in, Frame &out);
+        void encode(const Frame &in, std::vector<uint8_t> &out);
+        uint8_t CRC8_DVB_S2(const std::span<const uint8_t> data);
+
+    }
     namespace I
     {
         // Parent
